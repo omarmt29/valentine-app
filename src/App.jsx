03022825e9bd9ae7d61dc41ad6983fb992a07e08
unknown <omarmt0992@gmail.com>
2026-02-10 12:48:03 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './home'
 import Valentine from './valentine'
-import './app.css'
+import './App.css'
 
 export default function App() {
   return (
